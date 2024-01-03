69fdc636865d95ae2e2af6cece6e65e2a78f590b
SadNguyen2107 <nguyentrungson217@gmail.com>
2024-01-03 20:49:54 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SN_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            String str = "Hello World";
+
+            // Get input from user
+            char character;
+        askChar:
+            Console.Write("Enter the character: ");
+            bool isValid = Char.TryParse(Console.ReadLine(), out character);
+            if (!isValid)
+            {
+                goto askChar;
+            }
+
+            // Get the num of occurence
+            int count = ShowNumOfOccurrence(str, character);
+
+            Console.WriteLine($"Amount of {character} appear in {str}: {count} times");
+        }
+
+        private static int ShowNumOfOccurrence(string str, char character)
+        {
+            UInt16 count = 0;
+            for (int index = 0; index < str.Length; index++)
+            {
+                if (character == str[index])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
